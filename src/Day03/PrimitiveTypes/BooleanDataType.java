fbac5b7fc5b3b4b6dca2e234d73e274ae63993ae
azizkaraca <azizkaraca.07@gmail.com>
2022-11-15 11:56:45 +0300

    Java English Updated

---
+++ src/Day03/PrimitiveTypes/BooleanDataType.java
@@ -0,0 +1,15 @@
+package Day03.PrimitiveTypes;
+
+public class BooleanDataType {
+
+    public static void main(String[] args) {
+
+        boolean itIsRaining = false;
+        System.out.println(itIsRaining);
+
+        boolean isEligible = true;
+        System.out.println(isEligible);
+
+    }
+
+}
